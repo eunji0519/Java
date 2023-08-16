6bec4a394a7131492e1dc77eb390db10c6f453e3
eunji <kej980519@gmail.com>
2023-08-16 21:21:20 +0900

    first commit

---
+++ JavaTestRework/src/com/test/java/Item01.java
@@ -0,0 +1,16 @@
+package com.test.java;
+
+// 클래스명 > 의미 없이
+// ctrl + shift + L > quick search
+// 주석을 클래스 위에 다는 것은 지양
+
+public class Item01 {
+	public static void main(String[] args) {
+
+		// [SUMMARY] 변수만들기
+
+		int num; // 변수 생성 > 자료형 변수명
+
+		num = 100; // 변수 초기화
+	}
+}
